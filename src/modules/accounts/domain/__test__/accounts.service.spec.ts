3aefa93b9cd13a34977ff09ac73aa0ecf54c49b8
Eduardo Amaral <eduardofsamaral@gmail.com>
2021-09-11 21:53:29 -0300

    feat(BNK-001): create an account

--- src/modules/accounts/domain/__test__/accounts.service.spec.ts
+++ src/modules/accounts/domain/__test__/accounts.service.spec.ts
@@ -1,18 +1,23 @@
 import Customer from '../../../users/domain/customer';
 import CustomersService from '../../../users/domain/customers.service';
+import AccountsAdapter from '../../adapters/accounts.adapter';
+import Account from '../account';
 import AccountApplication from '../account-application';
 import { AccountsService } from '../accounts.service';
 
 describe('AccountsService', () => {
   let service: AccountsService;
   let customersService: CustomersService;
+  let accountsAdapter: AccountsAdapter;
 
   beforeEach(async () => {
     customersService = new CustomersService(null);
+    accountsAdapter = new AccountsAdapter();
 
     customersService.create = jest.fn();
+    accountsAdapter.create = jest.fn();
 
-    service = new AccountsService(customersService);
+    service = new AccountsService(customersService, accountsAdapter);
   });
 
   describe('create', () => {
@@ -30,6 +35,31 @@ describe('AccountsService', () => {
           name: 'Maria Silva',
           document: '1234',
           email: 'maria@mail.com',
+        }),
+      );
+    });
+
+    it('creates a new account', async () => {
+      const accountApplication = new AccountApplication({
+        name: 'Maria Silva',
+        document: '1234',
+        email: 'maria@mail.com',
+      });
+
+      const customer = new Customer({
+        id: 'uuid',
+        name: accountApplication.name,
+        email: accountApplication.email,
+        document: accountApplication.document,
+      });
+
+      customersService.create = jest.fn().mockResolvedValueOnce(customer);
+
+      await service.create(accountApplication);
+
+      expect(accountsAdapter.create).toHaveBeenCalledWith(
+        new Account({
+          customer,
         }),
       );
     });
--- src/modules/accounts/domain/accounts.service.ts
+++ src/modules/accounts/domain/accounts.service.ts
@@ -1,19 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import Customer from '../../users/domain/customer';
 import CustomersService from '../../users/domain/customers.service';
+import AccountsAdapter from '../adapters/accounts.adapter';
+import Account from './account';
 import AccountApplication from './account-application';
 
 @Injectable()
 export class AccountsService {
-  constructor(private customersService: CustomersService) {}
+  constructor(
+    private customersService: CustomersService,
+    private accountsAdapter: AccountsAdapter,
+  ) {}
 
   async create(accountApplication: AccountApplication): Promise<void> {
-    await this.customersService.create(
+    const customer = await this.customersService.create(
       new Customer({
         name: accountApplication.name,
         email: accountApplication.email,
         document: accountApplication.document,
       }),
     );
+
+    await this.accountsAdapter.create(new Account({ customer }));
   }
 }
